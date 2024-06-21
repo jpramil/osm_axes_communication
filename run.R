7dd80ab9a705324d7844949f6206fc52c7cc4c73
jpramil <julien.pramil@insee.fr>
2024-06-21 10:03:39 +0000

    update run

--- run.R
+++ run.R
@@ -63,11 +63,36 @@ m <- m %>%
   )
 
 
-# Export -----------------------------------------------------------------------
+# Exports -----------------------------------------------------------------------
 
 if (!dir.exists("output")) {
   dir.create("output")
 }
-output_file <- "output/lyon_map.html"
 
+output_file <- "output/lyon_map.html"
 htmlwidgets::saveWidget(m, file = output_file, selfcontained = TRUE)
+
+# Exporter les objets spatiaux dans le fichier GeoPackage
+
+BUCKET_OUT = "<mon_bucket>"
+FILE_KEY_OUT_S3 = "mon_dossier/BPE_ENS.csv"
+
+aws.s3::s3write_using(
+  lyon_autoroutes$osm_lines,
+  FUN = sf::st_write,
+  object = FILE_KEY_OUT_S3,
+  bucket = BUCKET_OUT,
+  opts = list("region" = "")
+)
+
+output_gpkg <- "output/lyon_axes_transports.gpkg"
+sf::st_write(lyon_autoroutes$osm_lines, dsn = output_gpkg, layer = "autoroutes", driver = "GPKG",delete_dsn = TRUE)
+sf::st_write(lyon_trunk$osm_lines, dsn = output_gpkg, layer = "trunk", driver = "GPKG", append = TRUE)
+sf::st_write(lyon_rail$osm_lines, dsn = output_gpkg, layer = "rail", driver = "GPKG", append = TRUE)
+sf::st_write(lyon_perrache$osm_points, dsn = output_gpkg, layer = "gare_perrache", driver = "GPKG", append = TRUE)
+sf::st_write(lyon_partdieu$osm_points, dsn = output_gpkg, layer = "gare_partdieu", driver = "GPKG", append = TRUE)
+sf::st_write(lyon_airports$osm_polygons, dsn = output_gpkg, layer = "airports", driver = "GPKG", append = TRUE)
+
+bucket_name <- "jpramil"
+output_file <- "BDTopo/lyon_axes_transports.gpkg"
+aws.s3::put_object(file = output_gpkg, bucket = bucket_name)
